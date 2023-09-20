803f7055493cd29a2a030904efc18d8023198529
Yusuke Ito <99148565+uskayyyyy@users.noreply.github.com>
2023-09-20 16:21:27 -0700

    Time: 44 ms (97.49%), Space: 45.1 MB (33.37%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.ts
+++ 2864-is-object-empty/2864-is-object-empty.ts
@@ -2,8 +2,5 @@ type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string
 type Obj = Record<string, JSONValue> | JSONValue[]
 
 function isEmpty(obj: Obj): boolean {
-    for (let x in obj) return false
-    return true
-    // if (Array.isArray(obj)) return !obj.length;
-    // return !Object.keys(obj).length;
+    return !Object.keys(obj).length;
 }
