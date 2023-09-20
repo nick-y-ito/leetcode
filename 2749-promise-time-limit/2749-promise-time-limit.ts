8d5b0b1f2876761e58efc45ce80b028f51b49ca0
Yusuke Ito <99148565+uskayyyyy@users.noreply.github.com>
2023-09-19 20:21:53 -0700

    Time: 48 ms (97.54%), Space: 42.6 MB (69.95%) - LeetHub

---
+++ 2749-promise-time-limit/2749-promise-time-limit.ts
@@ -0,0 +1,21 @@
+type Fn = (...params: any[]) => Promise<any>;
+
+const timeLimit = (fn: Fn, t: number): Fn => {
+    return (...args) => {
+        return new Promise(async (resolve, reject) => {
+            setTimeout(() => reject('Time Limit Exceeded'), t)
+            // try {
+            //     const result = await fn(...args);
+            //     resolve(result);
+            // } catch (e) {
+            //     reject(e);
+            // }
+            return fn(...args).then(resolve).catch(reject)
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
